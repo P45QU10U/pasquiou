2f373d0e039d1ace8c079b41d51ca7df996fdbd9
Richard Pasquiou <services@pasquiou.fr>
2021-01-06 19:02:20 +0100

    WIP : form contact

--- pages/index.js
+++ pages/index.js
@@ -11,6 +11,7 @@ import Base from '../components/index/Base'
 import Backend from '../components/index/Backend'
 import Quality from '../components/index/Quality'
 import Frameworks from '../components/index/Frameworks'
+import ContactForm from '../components/ContactForm'
 
 export async function getStaticProps() {
   const allPostsData = getSortedPostsData()
@@ -45,16 +46,14 @@ export default function Home({ allPostsData }) {
         <Backend />
         <Quality />
         <Frameworks />
-      </Container>
-      <Container>
+
         <Section className="relative z-10 grid grid-cols-1">
-          <div className="col-start-2 col-end-3 lg:col-start-1 lg:col-end-5 row-start-2 row-end-4 lg:row-end-5 lg:py-10 xl:py-16 flex -mr-8 pl-4 sm:mr-0 sm:pl-0" />
-          <div className="w-full -ml-full rounded-3xl transform shadow-lg bg-gradient-to-br from-orange-400 to-pink-500 -rotate-1 sm:-rotate-2">
-            <div className="p-5">
+          <div className="w-full -ml-full rounded-3xl shadow-lg bg-gradient-to-br from-orange-400 to-pink-500 transform -rotate-1 sm:-rotate-2 mb-4">
+            <div className="p-6">
               <h4>En vrac</h4>
               <p>J'ai fait peu de tests, mais je sais utiliser Jest, avec pour complément la React Testing Library.</p>
               <p>
-                Côté législation, j'ai tout lu le{' '}
+                Côté législation, j'ai décortiqué le{' '}
                 <abbr title="Réglement Général pour la Protection des Données">RGPD</abbr>.
               </p>
               <p>Enfin, je suis très intéressé par tout ce qui est création de sites statiques. NextJS est génial.</p>
@@ -63,13 +62,13 @@ export default function Home({ allPostsData }) {
             </div>
           </div>
         </Section>
-      </Container>
-      <Container>
-        <Section>
+
+        <Section className="enveloppevintage h-96">
           <blockquote>
             Je ne suis pas le mouton à 5 pattes, mais je pense à la transplantation. je suis investi, et m'efforce de
-            délivrer un code concis. Pour moi, ou les suivants… Si vous souhaitez m'embaucher, contactez-moi
+            délivrer un code concis. Pour moi, ou les suivants… Je suis ouvert à toute offre.
           </blockquote>
+          <ContactForm />
         </Section>
       </Container>
     </Skeleton>
