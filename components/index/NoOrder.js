5bd2f89ea11a84f1e13e632d974b836f454c193d
P45QU10U <services@pasquiou.fr>
2023-02-10 13:45:47 +0100

    updated test and typescript formulae

--- components/index/NoOrder.js
+++ components/index/NoOrder.js
@@ -6,13 +6,13 @@ const Frameworks = function () {
       <div className="w-full -ml-full rounded-3xl shadow-lg bg-gradient-to-br from-orange-400 to-pink-500 transform -rotate-1 sm:-rotate-2 mb-4">
         <div className="p-6">
           <h3>En vrac</h3>
-          <p>J'ai fait peu de tests, mais je sais utiliser Jest, avec pour complément la React Testing Library.</p>
+          <p>Je sais utiliser Jest, avec pour complément la React Testing Library.</p>
           <p>
             Côté législation, j'ai décortiqué le{' '}
             <abbr title="Réglement Général pour la Protection des Données">RGPD</abbr>.
           </p>
           <p>Enfin, je suis très intéressé par tout ce qui est création de sites statiques ; NextJS est génial.</p>
-          <p>TypeScript, je pratique, mais une bonne formation ne serait pas de refus.</p>
+          <p>TypeScript, je pratique, je me forme.</p>
           <code>// @ts-check</code>
           <blockquote>
             Je ne suis pas le mouton à 5 pattes. Je suis investi, et m'efforce de délivrer un code concis. Pour moi, ou
